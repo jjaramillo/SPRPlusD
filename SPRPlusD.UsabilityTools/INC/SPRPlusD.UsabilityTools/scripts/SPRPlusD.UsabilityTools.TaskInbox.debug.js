c13967aaa41d65348e3a7cbd303dfe34798eaeb3
Jaime Jaramillo <peleleman@msn.com>
2012-03-12 15:35:39 -0500

    Fixed some issues with the UI.

--- SPRPlusD.UsabilityTools/INC/SPRPlusD.UsabilityTools/scripts/SPRPlusD.UsabilityTools.TaskInbox.debug.js
+++ SPRPlusD.UsabilityTools/INC/SPRPlusD.UsabilityTools/scripts/SPRPlusD.UsabilityTools.TaskInbox.debug.js
@@ -8,18 +8,19 @@ SPRPlusD.UsabilityTools.TaskInbox.initialize = function () {
     var inboxtrigger = $('#inbox-trigger');
     var width = $(document).width() / 2;
     var tabs = $('#task-categories-tabs');
+    var triggerInitialOffset = -((inboxtrigger.width() / 2) - inboxtrigger.height() / 2);
 
-    inboxtrigger.css({ left: -inboxtrigger.outerHeight() - 15 });
+    inboxtrigger.css({ left: triggerInitialOffset });
     datacontainerbackground.css({ width: width, left: -width });
     datacontainer.css({ width: width, left: -width });
-    datacontainerbackground.css({ opacity: 0.5 });
+    datacontainerbackground.css({ opacity: 0.7 });
     opentrigger.click(
                     function () {
                         $(this).hide();
                         closetrigger.show();
                         datacontainerbackground.animate({ left: 0 });
                         datacontainer.animate({ left: 0 });
-                        inboxtrigger.animate({ left: width - inboxtrigger.outerHeight() - 15 })
+                        inboxtrigger.animate({ left: width + triggerInitialOffset })
                     }
                 );
     closetrigger.click(
@@ -28,7 +29,7 @@ SPRPlusD.UsabilityTools.TaskInbox.initialize = function () {
                         opentrigger.show();
                         datacontainerbackground.animate({ left: -width });
                         datacontainer.animate({ left: -width });
-                        inboxtrigger.animate({ left: -inboxtrigger.outerHeight() - 15 });
+                        inboxtrigger.animate({ left: triggerInitialOffset });
                     }
                 );
     tabs.find('a').each(function (index) {
