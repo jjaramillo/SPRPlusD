ca9adb739d3eb558c5b7c7702e27511c10b36bec
Jaime Jaramillo <peleleman@msn.com>
2012-03-13 16:25:37 -0500

    Basic BLL finished

--- SPRPlusD.UsabilityTools/BLL/TasksBLL.cs
+++ SPRPlusD.UsabilityTools/BLL/TasksBLL.cs
@@ -37,5 +37,82 @@ namespace SPRPlusD.UsabilityTools.BLL
             }
             return foundTasks;
         }
+
+        public static IEnumerable<Task> GetPendigCoolTasks(int coolOffsetDayCount, int warningOffsetDayCount, string siteUrl)
+        {
+            IEnumerable<SPListDTO> taskLists = SiteBLL.GetSiteTaskLists();
+            IEnumerable<Task> foundTasks = null;
+            DateTime coolOffsetDate;
+            using (RDDataContext dtxtc = new RDDataContext(siteUrl))
+            {
+                if (coolOffsetDayCount != 0 && warningOffsetDayCount != 0)
+                {
+                    if (warningOffsetDayCount > coolOffsetDayCount) { throw new ArgumentException("El rango de advertencia debe de ser menor que el rango de tareas pendientes"); }
+                    coolOffsetDate = DateTime.Now.Date.AddDays(coolOffsetDayCount);
+                }
+                else { coolOffsetDate = DateTime.Now.Date.AddDays(1); }
+                foreach (SPListDTO currentSPListDTO in taskLists)
+                {
+                    foundTasks = foundTasks == null ? dtxtc.GetList<Task>(currentSPListDTO.DisplayName).Where(
+                        task => task.DueDate == null || task.DueDate >= coolOffsetDate)
+                        : foundTasks.Union(dtxtc.GetList<Task>(currentSPListDTO.DisplayName).Where(
+                            task => task.DueDate == null || task.DueDate >= coolOffsetDate));
+                }
+            }
+            return foundTasks;
+        }
+
+        public static IEnumerable<Task> GetPendingWarningTasks(int coolOffsetDayCount, int warningOffsetDayCount, string siteUrl)
+        {
+            IEnumerable<SPListDTO> taskLists = SiteBLL.GetSiteTaskLists();
+            IEnumerable<Task> foundTasks = null;
+
+            using (RDDataContext dtxtc = new RDDataContext(siteUrl))
+            {
+                if (coolOffsetDayCount != 0 && warningOffsetDayCount != 0)
+                {
+                    if (warningOffsetDayCount > coolOffsetDayCount) { throw new ArgumentException("El rango de advertencia debe de ser menor que el rango de tareas pendientes"); }
+                    DateTime coolOffsetDate = DateTime.Now.Date.AddDays(coolOffsetDayCount);
+                    DateTime warningOffsetDate = DateTime.Now.Date.AddDays(warningOffsetDayCount);
+                    foreach (SPListDTO currentSPListDTO in taskLists)
+                    {
+                        foundTasks = foundTasks == null ? dtxtc.GetList<Task>(currentSPListDTO.DisplayName).Where(
+                            task => task.DueDate != null && (task.DueDate >= warningOffsetDate && task.DueDate < coolOffsetDate))
+                            : foundTasks.Union(dtxtc.GetList<Task>(currentSPListDTO.DisplayName).Where(
+                                task => task.DueDate != null && (task.DueDate >= warningOffsetDate && task.DueDate < coolOffsetDate)));
+                    }
+                }
+
+            }
+            return foundTasks;
+        }
+
+        public static IEnumerable<Task> GetPendingOverdueTasks(int coolOffsetDayCount, int warningOffsetDayCount, string siteUrl)
+        {
+            IEnumerable<SPListDTO> taskLists = SiteBLL.GetSiteTaskLists();
+            IEnumerable<Task> foundTasks = null;
+            DateTime warningOffsetDate;
+            using (RDDataContext dtxtc = new RDDataContext(siteUrl))
+            {
+                if (coolOffsetDayCount != 0 && warningOffsetDayCount != 0)
+                {
+                    if (warningOffsetDayCount > coolOffsetDayCount) { throw new ArgumentException("El rango de advertencia debe de ser menor que el rango de tareas pendientes"); }
+                    warningOffsetDate = DateTime.Now.Date.AddDays(warningOffsetDayCount);
+                }
+                else
+                {
+                    warningOffsetDate = DateTime.Now.Date;
+                }
+                foreach (SPListDTO currentSPListDTO in taskLists)
+                {
+                    foundTasks = foundTasks == null ? dtxtc.GetList<Task>(currentSPListDTO.DisplayName).Where(task => task.DueDate != null
+                            && (task.DueDate < warningOffsetDate && task.TaskStatus != TaskStatus.Completed))
+                        : foundTasks.Union(dtxtc.GetList<Task>(currentSPListDTO.DisplayName).Where(task => task.DueDate != null
+                            && (task.DueDate < warningOffsetDate && task.TaskStatus != TaskStatus.Completed)));
+                }
+
+            }
+            return foundTasks;
+        }
     }
 }
